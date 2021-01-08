eed894d74300f2a49c6febd5201922367c7f5981
Lars Noldan <lars@w9zeb.org>
2021-01-07 22:25:26 -0600

    My first live stream project

---
+++ Hangman/hangman.py
@@ -0,0 +1,79 @@
+import random
+import os
+
+# Set the list of words for the game & randomly select one.
+wordlist = ["hangman", "python", "computer", "dummy"]
+gameword = random.choice (wordlist)
+
+wordlen=len(gameword)
+allletters = list(gameword)
+
+# Gamestate variables
+drawing=[
+   """  ===|
+  |
+  |
+  |
+__|__ """,
+"""  ===|
+  |  O
+  |
+  |
+__|_""",
+"""  ===|
+  |  O
+  |  |
+  |  |
+__|_""",
+"""  ===|
+  |  O
+  | /|
+  |  |
+__|_""",
+"""  ===|
+  |  O
+  | /|\\
+  |  |
+__|_""",
+"""  ===|
+  |  O
+  | /|\\
+  |  |
+__|_/""",
+"""  ===|
+  |  O
+  | /|\\
+  |  |
+__|_/ \\"""]
+
+# Game output set inital value
+gameout = ["_"] * wordlen
+gamestate = 0
+guesses = []
+
+
+while gamestate < 6:
+    os.system('cls')
+    print(drawing[gamestate])
+    print("Your word has ", str(wordlen), " letters.")
+    print(*gameout)
+    print(*guesses)
+    guess = input("Please enter your guess:").lower()[0]
+    guesses.append(guess)
+    changed = False
+    for i,v in enumerate(allletters):
+        if guess == v:
+            changed = True
+            gameout[i] = v
+            print(*gameout)
+    if changed == False:
+        gamestate = gamestate +1
+        print(*gameout)
+    if "_" not in gameout:
+        print(drawing[gamestate])
+        print("You Win")
+        exit()
+
+print(drawing[6])
+print("Stop Doing It Wrong!")
+
