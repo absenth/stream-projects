a0699459cde441f9f066fb9d972874f4e24e5cb7
Lars <lars@w9zeb.org>
2021-01-27 15:25:08 -0600

    Logic improvements

--- training-aid/training.py
+++ training-aid/training.py
@@ -1,5 +1,6 @@
 import os
 import random
+import time
 
 targets = [
         "A", "B", "C", "D", "E", 
@@ -10,10 +11,10 @@ itteration = int(input("Please enter the number of itterations: "))
 
 while itteration > 0:
   targetcount = random.randint(1, 5)
+  input("press enter when ready")
   while targetcount > 0:
     random_index = random.randrange(len(targets))
     os.system('echo "{0}" | festival --tts'.format(random_index)) 
     targetcount = targetcount - 1 
-  input("Press Enter to continue... ")
   itteration = itteration - 1
 
