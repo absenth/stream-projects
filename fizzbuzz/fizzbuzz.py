d4f8b307b2e1416aec55d8865e9728d8cb7c8891
absenth <wintermute223@gmail.com>
2021-05-02 11:49:13 -0500

    First working attempt at solving fizzbuzz problem

---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,12 @@
+# first attempt ever at solving FizzBuzz
+# https://wiki.c2.com/?FizzBuzzTest
+
+for n in range(100):
+    if (n+1) %3 == 0 and (n+1) %5 == 0:
+        print("fizzbuzz")
+    elif (n+1) %3 == 0:
+        print("fizz")
+    elif (n+1) %5 == 0:
+        print("buzz")
+    else:
+        print(n+1)
