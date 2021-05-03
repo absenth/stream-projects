e99451a4544faa6485ba2af682e4e007f5270dd5
Absenth <lars@w9zeb.org>
2021-05-03 07:11:25 -0500

    reset to 100

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,7 +1,7 @@
 # first attempt ever at solving FizzBuzz
 # https://wiki.c2.com/?FizzBuzzTest
 
-for n in range(100000):
+for n in range(100):
     if (n+1) %3 == 0 and (n+1) %5 == 0:
         print("fizzbuzz")
     elif (n+1) %3 == 0:
