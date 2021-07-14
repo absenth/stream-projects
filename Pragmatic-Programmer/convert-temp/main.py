33163e551244ee81ed217f47cefbf63f268a89d5
absenth <wintermute223@gmail.com>
2021-07-14 10:08:27 -0500

    refactoring for functions

--- Pragmatic-Programmer/convert-temp/main.py
+++ Pragmatic-Programmer/convert-temp/main.py
@@ -4,13 +4,24 @@
     Formula is: C = (F - 32) / 1.8
 """
 
-known_Temperature = input("Input Temperature.  ie:'27c' ")
+def main():
+    """Take a temperature as input and feed that to the conversion  function"""
+    known_Temperature = input("Input Temperature. ie:'27c' ")
 
-if known_Temperature[-1].lower() == "f":
-    converted_Temperature = (float(known_Temperature[0: -1]) - 32) / 1.8
-    print(f"{known_Temperature} is {converted_Temperature}c")
-elif known_Temperature[-1].lower() == "c":
-    converted_Temperature = (float(known_Temperature[0: -1]) * 1.8) + 32
-    print(f"{known_Temperature} is {converted_Temperature}f")
-else:
-    print("I can't convert your temperature without a unit")
+    converted_Temperature = convert_Temp(known_Temperature)
+    print(converted_Temperature)
+
+def convert_Temp(temp_Input):
+    """Take a temperature as input, and convert it"""
+    if temp_Input[-1].lower() == "f":
+        converted_Temperature = (float(temp_Input[0: -1]) - 32) / 1.8
+        return(f"{converted_Temperature}c")
+    elif temp_Input[-1].lower() == "c":
+        converted_Temperature = (float(temp_Input[0: -1]) * 1.8) + 32
+        return(f"{converted_Temperature}f")
+    else:
+        print("I can't convert your temperature without a unit")
+
+
+if __name__ == "__main__":
+    main()
