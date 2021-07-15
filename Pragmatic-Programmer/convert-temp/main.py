18d7df9f7617c08984afd000819608cd282b03c3
Absenth <lars@w9zeb.org>
2021-07-15 02:06:20 +0000

    Input sanitization branch (#5)
    * Adding some input sanitization checks

--- Pragmatic-Programmer/convert-temp/main.py
+++ Pragmatic-Programmer/convert-temp/main.py
@@ -4,23 +4,43 @@
     Formula is: C = (F - 32) / 1.8
 """
 
+
+import re
+
+
 def main():
     """Take a temperature as input and feed that to the conversion  function"""
-    known_Temperature = input("Input Temperature. ie:'27c' ")
+    KnownTemperature = input("Input Temperature. ie:'27c' ")
 
-    converted_Temperature = convert_Temp(known_Temperature)
-    print(converted_Temperature)
+    if CheckInput(KnownTemperature):
+        ConvertedTemperature = ConvertTemp(KnownTemperature)
+        Temp = ConvertedTemperature[0: -1]
+        Unit = ConvertedTemperature[-1]
+        Temp = round(float(Temp), 2)
+        print(f"{Temp}{Unit}")
 
-def convert_Temp(temp_Input):
+
+def ConvertTemp(input):
     """Take a temperature as input, and convert it"""
-    if temp_Input[-1].lower() == "f":
-        converted_Temperature = (float(temp_Input[0: -1]) - 32) / 1.8
-        return(f"{converted_Temperature}c")
-    elif temp_Input[-1].lower() == "c":
-        converted_Temperature = (float(temp_Input[0: -1]) * 1.8) + 32
-        return(f"{converted_Temperature}f")
+    if input[-1].lower() == "f":
+        ConvertedTemperature = (float(input[0: -1]) - 32) / 1.8
+        return(f"{ConvertedTemperature}c")
+    elif input[-1].lower() == "c":
+        ConvertedTemperature = (float(input[0: -1]) * 1.8) + 32
+        return(f"{ConvertedTemperature}f")
     else:
         print("I can't convert your temperature without a unit")
+
+
+def CheckInput(input):
+    if '.' in input:
+        check = bool(re.match(r"^\d+\.\d+[cCfF]$", input))
+    else:
+        check = bool(re.match(r"^\d+[cCfF]$", input))
+    if check is False:
+        print("The value you input didn't match the expected format. Please try again.")
+        main()
+    return check
 
 
 if __name__ == "__main__":
