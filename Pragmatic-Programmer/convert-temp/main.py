bad0c0ac5d4e1e3bb61ca52007a78271f180e49d
absenth <wintermute223@gmail.com>
2021-07-14 20:32:34 -0500

    Adding some input sanitization checks

--- Pragmatic-Programmer/convert-temp/main.py
+++ Pragmatic-Programmer/convert-temp/main.py
@@ -4,23 +4,38 @@
     Formula is: C = (F - 32) / 1.8
 """
 
+
+import re
+
+
 def main():
     """Take a temperature as input and feed that to the conversion  function"""
-    known_Temperature = input("Input Temperature. ie:'27c' ")
+    KnownTemperature = input("Input Temperature. ie:'27c' ")
 
-    converted_Temperature = convert_Temp(known_Temperature)
-    print(converted_Temperature)
+    if CheckInput(KnownTemperature):
+        ConvertedTemperature = ConvertTemp(KnownTemperature)
 
-def convert_Temp(temp_Input):
+        print(ConvertedTemperature)
+
+
+def ConvertTemp(temp_Input):
     """Take a temperature as input, and convert it"""
     if temp_Input[-1].lower() == "f":
-        converted_Temperature = (float(temp_Input[0: -1]) - 32) / 1.8
-        return(f"{converted_Temperature}c")
+        ConvertedTemperature = (float(temp_Input[0: -1]) - 32) / 1.8
+        return(f"{ConvertedTemperature}c")
     elif temp_Input[-1].lower() == "c":
-        converted_Temperature = (float(temp_Input[0: -1]) * 1.8) + 32
-        return(f"{converted_Temperature}f")
+        ConvertedTemperature = (float(temp_Input[0: -1]) * 1.8) + 32
+        return(f"{ConvertedTemperature}f")
     else:
         print("I can't convert your temperature without a unit")
+
+
+def CheckInput(input):
+    check = bool(re.match(r"^\d+[cCfF]$", input))
+    if check is False:
+        print("The value you input didn't match the expected format. Please try again.")
+        main()
+    return check
 
 
 if __name__ == "__main__":
