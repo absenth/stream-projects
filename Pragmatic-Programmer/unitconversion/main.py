fe4421e2886ce38fe058dfaef1bea4ceddcca70d
absenth <1115138+absenth@users.noreply.github.com>
2021-07-25 14:11:01 -0500

    adding check_input logic that works

--- Pragmatic-Programmer/unitconversion/main.py
+++ Pragmatic-Programmer/unitconversion/main.py
@@ -12,41 +12,39 @@ def main():
     """Take user input, including a unit and convert it to it's partner unit"""
 
     print("Please Provide a Value and Unit to convert.")
-    print("ie: 27c for celcius, or 75m for miles")
-    user_value = ""
+    print("ie: 27c for celsius, or 75m for miles")
     user_value = input("> ")
 
     # validate that the user input a proper value
-    check_input(user_value)
-
-    # detect the expecte conversion and execute
-    value, unit = detect_unit(user_value)
-    if unit == "c":
-        temp = round(float(c2f(value)), 2)
-        print(f"Temperature is {temp} degrees Fahrenheit")
-    elif unit == "f":
-        temp = round(float(f2c(value)), 2)
-        print(f"Temperature is {temp} degrees Celcius")
-    elif unit == "m":
-        distance = round(float(m2km(value)), 2)
-        print(f"Distance is {distance} kilometers")
-    elif unit == "km":
-        distance = round(float(km2m(value)), 2)
-        print(f"Distance is {distance} miles")
-    elif unit == "g":
-        volume = round(float(g2l(value)), 2)
-        print(f"Volume is {volume} liters")
-    elif unit == "l":
-        volume = round(float(l2g(value)), 2)
-        print(f"Volume is {volume} gallons")
-    elif unit == "p":
-        weight = round(float(p2kg(value)), 2)
-        print(f"Weight is {weight} kilograms")
-    elif unit == "kg":
-        weight = round(float(kg2p(value)), 2)
-        print(f"Weight is {weight} pounds")
-    else:
-        invalid_input(user_value)
+    if check_input(user_value):
+        # detect the expecte conversion and execute
+        value, unit = detect_unit(user_value)
+        if unit == "c":
+            temp = round(float(c2f(value)), 2)
+            print(f"Temperature is {temp} degrees Fahrenheit")
+        elif unit == "f":
+            temp = round(float(f2c(value)), 2)
+            print(f"Temperature is {temp} degrees Celcius")
+        elif unit == "m":
+            distance = round(float(m2km(value)), 2)
+            print(f"Distance is {distance} kilometers")
+        elif unit == "km":
+            distance = round(float(km2m(value)), 2)
+            print(f"Distance is {distance} miles")
+        elif unit == "g":
+            volume = round(float(g2l(value)), 2)
+            print(f"Volume is {volume} liters")
+        elif unit == "l":
+            volume = round(float(l2g(value)), 2)
+            print(f"Volume is {volume} gallons")
+        elif unit == "p":
+            weight = round(float(p2kg(value)), 2)
+            print(f"Weight is {weight} kilograms")
+        elif unit == "kg":
+            weight = round(float(kg2p(value)), 2)
+            print(f"Weight is {weight} pounds")
+        else:
+            invalid_input(user_value)
 
 
 def check_input(input):
@@ -122,7 +120,7 @@ def invalid_input(input):
     print(f"You input {input} which wasn't understood.")
     print("Please input a value like 27c")
     print("Units this program understands are:")
-    print("c = celcius / f = fahrenheit / m = miles / km = kilometers")
+    print("c = celsius / f = fahrenheit / m = miles / km = kilometers")
     print("g = gallons / l = liters / p = poundsa / kg = kilograms")
     print("")
 
