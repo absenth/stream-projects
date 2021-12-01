2cb20e13f9954060fdd531f9072bd428b5495e53
absenth <1115138+absenth@users.noreply.github.com>
2021-11-30 22:03:38 -0600

    Trying to glue my TAU Websocket client to my Discord bot

--- Discord-Bot/main.py
+++ Discord-Bot/main.py
@@ -4,7 +4,7 @@ import random
 import asyncio
 import websockets
 import json
-
+import tauclient
 user_token = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
@@ -35,4 +35,15 @@ async def on_message(message):
         return
 
 
-client.run(user_token)
+async def run_all():
+    discord_client = client.start(user_token)
+    notification_channel = await client.fetch_channel(707230298714275872)
+    tauclient_client = tauclient.listen(notification_channel)
+    await asyncio.gather(discord_client, tauclient_client)
+
+asyncio.run(run_all()) # no need to explicitly get the event loop past 3.7 or 3.8
+
+# Bot "goes online" when the script is run
+# Bot does not respond to keywords
+# Bot does not respond to `Stream-online` events
+# https://stackoverflow.com/questions/66197394/client-get-channelid-returning-none-on-a-channel-that-exists/66197697#66197697
