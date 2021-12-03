911bcd11fc9326ac369fc9ef4f965990856f4b5b
Absenth <1115138+absenth@users.noreply.github.com>
2021-12-02 22:29:17 -0600

    Discord twitch asyncio troubleshooting (#15)
    * Trying to glue my TAU Websocket client to my Discord bot
    
    * Working on tying Discord to Twitch via TAU

--- Discord-Bot/tauclient.py
+++ Discord-Bot/tauclient.py
@@ -4,7 +4,7 @@ import os
 import json
 from pprint import pprint
 
-async def listen():
+async def listen(channel):
     url = 'ws://192.168.1.204:8000/ws/twitch-events/'
     tau_token = os.getenv('TAU_TOKEN')
     json_data = '{ \"token\": \"%s\" }'%tau_token
@@ -17,7 +17,12 @@ async def listen():
             event_type = data['event_type']
             streamer = data['event_data']['broadcaster_user_name']
             if event_type == 'stream-online':
-                print(f'{streamer} has just gone live.  Check them out at https://twitch.tv/{streamer}')
+                channel.send(f'{streamer} has just gone live.  Check them out at https://twitch.tv/{streamer}')
 
+''' I don't know if this script/function knows how to send.
+see the `error` file in this folder for an example traceback
 
-asyncio.get_event_loop().run_until_complete(listen())
+That should work, but you'll probably need "await channel.send(...)"; however,
+the error means it is trying to do None.send, and that None comes from the
+client.get_channel not finding the channel according to the doc
+'''
--- Stream-Helper/main.py
+++ Stream-Helper/main.py
@@ -5,9 +5,11 @@ import json
 from pprint import pprint
 
 base_url = os.getenv('NGROK_TAU_URL')
+tau_token = os.getenv('TAU_TOKEN')
+
 query_path = 'api/v1/twitch-events'
 #query_path = 'api/v1/heartbeat'
-tau_token = os.getenv('TAU_TOKEN')
+#query_path = f"api/v1/twitch/helix-endpoints/"
 
 headers = {
         "Authorization": f"Token {tau_token}",
@@ -17,7 +19,9 @@ def main():
     response = requests.get(f"{base_url}/{query_path}", headers=headers)
     x = response.json()
     stream_online = x["results"][0]["event_type"]
-    pprint(stream_online)
+    if stream_online == 'stream-online':
+        pprint(stream_online)
+
 
 if __name__ == '__main__':
     main()
