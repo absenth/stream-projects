659debf39011b1100b4c6dea6eb3a78e9baedc3f
absenth <1115138+absenth@users.noreply.github.com>
2022-01-03 08:29:04 -0600

    This works for locations not in north or south America

--- Stream-Chat/geocage.py
+++ Stream-Chat/geocage.py
@@ -6,7 +6,7 @@ import os
 def geocode_lookup(city):
     api_key = os.getenv('OPENCAGE_API_KEY')
     base_url = "http://api.opencagedata.com/geocode/v1/geojson?"
-    complete_url = base_url + "&q=" + city + "&key=" + api_key + "&limit=1"
+    complete_url = f"{base_url}&q={city}&key={api_key}&limit=1"
     response = requests.get(complete_url).json()
     coords = (response['features'][0]['geometry']['coordinates'])
     lat=coords[0]
@@ -15,5 +15,5 @@ def geocode_lookup(city):
 
 
 if __name__ == "__main__":
-    lat,lng = geocode_lookup("austin")
+    lat,lng = geocode_lookup("london")
     print(f"Lat: {lat}  Lng: {lng}")
