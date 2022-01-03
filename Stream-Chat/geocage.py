fa01412e63dd769799faf82426fe5a07163e99b2
absenth <1115138+absenth@users.noreply.github.com>
2022-01-03 13:23:05 -0600

    I'm an idiot, and swapped the lat/lon values

--- Stream-Chat/geocage.py
+++ Stream-Chat/geocage.py
@@ -9,11 +9,12 @@ def geocode_lookup(city):
     complete_url = f"{base_url}&q={city}&key={api_key}&limit=1"
     response = requests.get(complete_url).json()
     coords = (response['features'][0]['geometry']['coordinates'])
-    lat=coords[0]
-    lng=coords[1]
+    lat=coords[1]
+    lng=coords[0]
+    print(response['features'])
     return(lat,lng)
 
 
 if __name__ == "__main__":
-    lat,lng = geocode_lookup("london")
+    lat,lng = geocode_lookup("austin")
     print(f"Lat: {lat}  Lng: {lng}")
