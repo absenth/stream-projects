fd5c5b1fd6bc4da685b2abf0adcff6787a3849f4
absenth <1115138+absenth@users.noreply.github.com>
2022-08-06 08:02:35 -0500

    updates

--- bowling/bowling/main.py
+++ bowling/bowling/main.py
@@ -19,5 +19,19 @@ def main():
         print("DB Already Exists")
 
 
+
+def add_game():
+    bowler = input("Bowler's name: ").lower()
+    utcdate = input("Date of Game: ")
+    game = input("Which game is this: ")
+    f1b1 = input("How many pins first ball frame 1: ")
+    if int(f1b1) not 10:
+        split = input("Was there a split: ")
+        if split.lower == "yes"
+            f1s = input("which pins were left: ie. 6,7,10 ")
+        f1b2 = input("How many pins second ball frame 1: ")
+
+
+
 if __name__ == "__main__":
     main()
