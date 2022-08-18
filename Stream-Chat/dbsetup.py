f698e97ad9a27a100743531289f97b598f5fe40b
absenth <1115138+absenth@users.noreply.github.com>
2022-08-18 05:54:58 -0500

    starting to setup sqlite3 to replaces commands.csv

---
+++ Stream-Chat/dbsetup.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+
+dbname = "commandsdb"
+
+
+def db_connect():
+    con = sqlite3.connect(dbname)
+    return con
+
+con = db_connect()
+cur = con.cursor()
+
+
+def main():
+    print(f"please don't call this script directly")
+
+
+def check_for_db():
+    cur.execute("SELECT name from sqlite_master WHERE type='table'")
+    return(cur.fetchall())
+
+
+def setup_commands_db():
+    cur.execute('''CREATE TABLE IF NOT EXISTS commands
+        ([pk] INTEGER PRIMARY KEY NOT NULL,
+        [command] TEXT, [commandout], TEXT''')
+
+    return True
+
+
+def insert_new_command(cur, newcommand):
+    sql = '''INSERT INTO commands(command, commandout)
+             VALUES(?, ?) '''
+    cur.execute(sql, newcommand)
+    con.commit()
+    return cur.lastrowid
+
+
+def add_command():
+    new_command = input("What command would you like to add? ").lower()
+    new_commandout = input("What should this command return? ")
+    newcommand = (new_command, newcommandout)
+    insert_new_command(con, newcommand)
+
+
+if __name__ == "__main__":
+    main()
