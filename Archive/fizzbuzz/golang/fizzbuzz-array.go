c1cbb0ca2f87a9fd1d5c2d62ac75e54b2f12bb1a
absenth <1115138+absenth@users.noreply.github.com>
2024-05-01 03:10:12 -0500

    updating the array version to print each element on it's own line

--- Archive/fizzbuzz/golang/fizzbuzz-array.go
+++ Archive/fizzbuzz/golang/fizzbuzz-array.go
@@ -9,13 +9,15 @@ func main() {
 	var s []string
 	for i := 1; i < 2000001; i++ {
 		if i%3 == 0 && i%5 == 0 {
-			s = append(s, "fizzbuzz")
+			s = append(s, "fizzbuzz \n")
 		} else if i%3 == 0 {
-			s = append(s, "fizz")
+			s = append(s, "fizz \n")
 		} else if i%5 == 0 {
-			s = append(s, "buzz")
+			s = append(s, "buzz \n")
 		} else {
-			s = append(s, strconv.Itoa(i))
+			n := strconv.Itoa(i)
+			o := fmt.Sprintf("%s \n", n)
+			s = append(s, o)
 		}
 	}
 	fmt.Println(s)
