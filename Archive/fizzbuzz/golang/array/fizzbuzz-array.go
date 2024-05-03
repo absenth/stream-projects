d54d03b50287ba098669907132dbe960a9fdb8e5
absenth <1115138+absenth@users.noreply.github.com>
2024-05-03 12:54:20 -0500

    Slices, arrays, and standard oh my

--- Archive/fizzbuzz/golang/array/fizzbuzz-array.go
+++ Archive/fizzbuzz/golang/array/fizzbuzz-array.go
@@ -6,18 +6,18 @@ import (
 )
 
 func main() {
-	var s []string
+	var s [2000000]string
 	for i := 1; i < 2000001; i++ {
 		if i%3 == 0 && i%5 == 0 {
-			s = append(s, "fizzbuzz \n")
+			s[i-1] = "fizzbuzz \n"
 		} else if i%3 == 0 {
-			s = append(s, "fizz \n")
+			s[i-1] = "fizz \n"
 		} else if i%5 == 0 {
-			s = append(s, "buzz \n")
+			s[i-1] = "buzz \n"
 		} else {
 			n := strconv.Itoa(i)
 			o := fmt.Sprintf("%s \n", n)
-			s = append(s, o)
+			s[i-1] = o
 		}
 	}
 	fmt.Println(s)
---
+++ Archive/fizzbuzz/golang/slice/fizzbuzz-slice.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var s = make([]string, 0, 2000000)
+	for i := 1; i < 2000001; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			s = append(s, "fizzbuzz \n")
+		} else if i%3 == 0 {
+			s = append(s, "fizz \n")
+		} else if i%5 == 0 {
+			s = append(s, "buzz \n")
+		} else {
+			n := strconv.Itoa(i)
+			o := fmt.Sprintf("%s \n", n)
+			s = append(s, o)
+		}
+	}
+	fmt.Println(s)
+}
